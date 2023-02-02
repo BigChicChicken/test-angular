b44b1e582cf4db8330ab9cc3a99f398e51213694
Florent TEDESCO <florent.tedesco@sogexia.com>
2023-02-02 16:56:32 +0100

    Remove useless error

--- app/src/app/@errors/character-parse/character-parse.error.ts
+++
@@ -1,6 +0,0 @@
-export class CharacterParseError extends Error {
-    constructor(value: string) {
-        super(`"${value}" isn't a valid JSON.`);
-        this.name = 'CharacterParseError';
-    }
-}
